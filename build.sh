014a6a816df8ce1dc71b1d4f5b1bdf3f187e28a5
Matt Revelle <matthew.revelle@montana.edu>
2026-08-26 16:10:03 -0600

    Deploy course site with GitHub Pages

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -euo pipefail
 
 cd csci_591/res
 /bin/bash mktable.sh
 cd ../..
-uv run mkdocs build -f csci_591/mkdocs.yml
+uv run --locked mkdocs build --strict -f csci_591/mkdocs.yml
